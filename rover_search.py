9ad803c24c43de1476484ee495d8f3a00d400372
Fraida Fund <ffund@nyu.edu>
2024-08-09 14:03:19 -0400

    Update saved file position

--- rover_search.py
+++ rover_search.py
@@ -51,12 +51,10 @@ class RoverSearch(StateMachine):
     heading_seq_w_idx = 0
 
     measurement_list = []
-    # start at the SE bound
+
+    # start in the middle of the search space
     next_waypoint = {'lat': (MIN_LAT + MAX_LAT)/2, 'lon': (MIN_LON + MAX_LON)/2}
 
-    # See https://github.com/bayesian-optimization/BayesianOptimization/blob/master/examples/advanced-tour.ipynb
-    # Note: using sequential domain reduction to improve convergence time
-    # https://github.com/bayesian-optimization/BayesianOptimization/blob/master/examples/domain_reduction.ipynb
     optimizer = BayesianOptimization(
       f=None,
       pbounds={'lat': (MIN_LAT, MAX_LAT), 'lon': (MIN_LON, MAX_LON)},
@@ -65,12 +63,11 @@ class RoverSearch(StateMachine):
       allow_duplicate_points=True
     )
 
-    # Note: change this utility function to manage the 
+    # change this utility function to manage the 
     # exploration/exploitation tradeoff
-    # see: https://github.com/bayesian-optimization/BayesianOptimization/blob/master/examples/exploitation_vs_exploration.ipynb
     utility = UtilityFunction(kind="ucb", kappa=1)
 
-    # set the kernel, alpha parameter
+    # set the kernel
     kernel = RBF()
     optimizer._gp.set_params(kernel = kernel, normalize_y=True)
 
@@ -96,12 +93,6 @@ class RoverSearch(StateMachine):
             default=defaultFile,
         )
         parser.add_argument(
-            "--model",
-            help="File to record model at the end",
-            required=False,
-            default=defaultModelFile,
-        )
-        parser.add_argument(
             "--save_csv",
             help="Whether to save a file with the search path and RSSI values",
             default=True,
@@ -125,14 +116,13 @@ class RoverSearch(StateMachine):
         if self.save_csv:
             self.csv_writer = csv.writer(self.log_file)
             self.csv_writer.writerow(["timestamp","longitude", "latitude", "altitude", "RSSI", "best_lat", "best_lon", "best_RSSI"])
-
+        
     @state(name="start", first=True)
     async def start(self, vehicle: Drone):
         # record the start time of the search
         await vehicle.takeoff(SEARCH_ALTITUDE)
         self.start_time = datetime.datetime.now()
         print("Reached altitude, starting search.")
-
 
         return "probe"
 
@@ -149,7 +139,6 @@ class RoverSearch(StateMachine):
 
         self.best_measurement = gpr_mean.max()
         self.best_pos = Coordinate(COORD_ARRAY[np.argmax(gpr_mean)][0],COORD_ARRAY[np.argmax(gpr_mean)][1], SEARCH_ALTITUDE)
-
 
         # save the measurements and estimates if logging to file
         if self.best_pos and self.save_csv:
@@ -175,7 +164,7 @@ class RoverSearch(StateMachine):
     async def suggest(self, vehicle: Drone):
         # suggest the next waypoint to visit
         self.next_waypoint = self.optimizer.suggest(self.utility)
-        # go to next waypoint
+
         return "probe"
     
 
@@ -185,7 +174,7 @@ class RoverSearch(StateMachine):
         if datetime.datetime.now() - self.start_time > self.search_time:
             return "end"
 
-        # go to the waypoint, probe along the way
+        # go to the waypoint
         next_pos =  Coordinate(
             np.clip(self.next_waypoint['lat'], MIN_LAT, MAX_LAT),
             np.clip(self.next_waypoint['lon'], MIN_LON, MAX_LON),
@@ -194,6 +183,7 @@ class RoverSearch(StateMachine):
         moving = asyncio.ensure_future(
             vehicle.goto_coordinates(next_pos)
         )
+        
         while not moving.done():
             await asyncio.sleep(0.2)
 
@@ -217,6 +207,7 @@ class RoverSearch(StateMachine):
     @state(name="end")
     async def end(self, vehicle: Drone):
         # Return vehicle to start and land it
+
         print(
             f"Search time of {self.search_time} minutes has elapsed. Returning to launch!"
         )
@@ -224,14 +215,13 @@ class RoverSearch(StateMachine):
             f"Best rover location estimate {self.best_pos.lat, self.best_pos.lon} with measurement {self.best_measurement} after {datetime.datetime.now()-self.start_time} minutes"
         )
         # save the final model
-        with open(args.model, 'wb') as handle:
+        with open("/root/Results/gpr_fitted_model.pickle", 'wb') as handle:
             pickle.dump(self.optimizer._gp, handle)
 
+        # go home
         home_coords = Coordinate(
             vehicle.home_coords.lat, vehicle.home_coords.lon, vehicle.position.alt
         )
-
-
         await vehicle.goto_coordinates(home_coords)
         await vehicle.land()
         print("Done!")
